55d6ce8a75437a47d4fb0b11883f778b16067240
Alex <revolalex@gmail.com>
2021-02-26 16:27:08 +0100

    style neon nav

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg" type="dark" variant="dark">
       <nuxt-link to="/">🍸</nuxt-link>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -33,9 +33,20 @@ export default {
 <style>
 /* exact link will show the primary color for only the exact matching link */
 a.nuxt-link-exact-active {
-  color: #ffffff;
+  color: #faf705;
+  text-shadow: 0 0 5px #dddddd, 0 0 10px #a5f1ff, 0 0 20px #000000,
+    0 0 30px #d8fdd5, 0 0 40px #d8fdd5;
 }
 a {
-  color: #000000;
+  letter-spacing: 0.2rem;
+  font-size: 1.1rem;
+  color: #fc2192;
+  text-shadow: 0 0 1px #f700b9, 0 0 1px #a5f1ff, 0 0 2px #b75e8c,
+    0 0 3px #f06aaf, 0 0 40px #b75e8c;
+}
+a:hover {
+  color: #faf705;
+  text-shadow: 0 0 5px #dddddd, 0 0 10px #a5f1ff, 0 0 20px #000000,
+    0 0 30px #d8fdd5, 0 0 40px #d8fdd5;
 }
 </style>
